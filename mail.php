2616e55c6e20f4ef9c0fd28f568144c5fbb38c41
King-16 <navroczkijdima@gmail.com>
2019-02-18 20:34:51 +0200

    proba

--- mail.php
+++ mail.php
@@ -1,14 +1,32 @@
-<?php 
-// если была нажата кнопка "Отправить" 
-if($_POST['submit']) {
-        $title = substr(htmlspecialchars(trim($_POST['title'])), 0, 1000); 
-        $mess =  substr(htmlspecialchars(trim($_POST['mess'])), 0, 1000000); 
-        // $to - кому отправляем 
-        $to = 'navroczkijdima@mail.ru'; 
-        // $from - от кого 
-        $from='duduve@netmails.info'; 
-        // функция, которая отправляет наше письмо
-        mail($to, $title, $mess, 'From:'.$from); 
-        echo 'Спасибо! Ваше письмо отправлено.'; 
-} 
+<html>
+<head>
+<title>Форма заявки с сайта</title>
+</head>
+<body>
+<?php
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['fio']) and !isset($_POST['email'])){
+ ?> <form action="send.php" method="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form> <?php
+} else {
+ //показываем форму
+ $fio = $_POST['fio'];
+ $email = $_POST['email'];
+ $fio = htmlspecialchars($fio);
+ $email = htmlspecialchars($email);
+ $fio = urldecode($fio);
+ $email = urldecode($email);
+ $fio = trim($fio);
+ $email = trim($email);
+ if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){ 
+ echo "Сообщение успешно отправлено"; 
+ } else { 
+ echo "При отправке сообщения возникли ошибки";
+ }
+}
 ?>
+</body>
+</html>
